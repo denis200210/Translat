c99675e4fe7e3de17ec9d9691eaf54101dca8433
denis200210 <71909269+denis200210@users.noreply.github.com>
2021-01-15 03:49:16 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main() {
     cout <<"Input text (а..я): ";// вводим текст
     cin.getline(str,500);// считываем строку
     cout << "Output: ";
-    translate(str, angl, rus); // наш написанный транслит
+    translate(str, angl, rus);  // наш написанный транслит
     delete [] str;// очищаем память
     return 0;
 }
