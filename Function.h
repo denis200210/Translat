825842a03c039dadc75d47beebd29615447f09e9
denis200210 <71909269+denis200210@users.noreply.github.com>
2021-01-15 04:53:28 +0900

    Update Function.h

--- Function.h
+++ Function.h
@@ -5,6 +5,6 @@ char* str = new char[500];
 const char* angl = "ABVGDEIoGZIIKLMNOPRSTYFHCChShShEUYaabvgdeiogziiklmnoprstyfhcchshshyieuyai'";// массив англ букв
 const char* rus = "АБВГДЕЁ/ЖЗИЙКЛМНОПРСТУФХЦЧ/Ш/Щ/ЭЮЯ/бвгдеё/жзийклмнопрстуфхцч/ш/щ/ы/эюя/ьъ.";// массив русских букв
 
-void translate(const char *str, const char *angl, const char *rus);// используем указатель
+void trans(const char *str, const char *angl, const char *rus);// используем указатель
 
 #endif
